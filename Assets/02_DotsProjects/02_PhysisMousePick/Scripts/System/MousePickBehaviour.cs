c541db0286f6476dc9569d1cd7791adc1762659b
jayIndieGame <17635351002@163.com>
2021-07-16 23:30:51 +0800

    add physics drag dots and tradition way

--- Assets/02_DotsProjects/02_PhysisMousePick/Scripts/System/MousePickBehaviour.cs
+++ Assets/02_DotsProjects/02_PhysisMousePick/Scripts/System/MousePickBehaviour.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Assertions;
@@ -12,6 +13,7 @@ using UnityEngine;
 using static Unity.Physics.Math;
 
 //整个脚本基本和官方的UnityPhysicsSamples中的MousePick一致。这个MousePick是个不错的范例脚本。
+//直接就照着抄了。一些注释会写的随意一点
 namespace Unity.Physics.MousePickBehaviour
 {
     public class MousePickBehaviour : MonoBehaviour, IConvertGameObjectToEntity
@@ -28,6 +30,7 @@ namespace Unity.Physics.MousePickBehaviour
         }
     }
     //鼠标每次点击都会将点击储存起来。
+    //很标准的一个ICollector<RaycastHit> 没有添加任何额外的功能。。为啥不做成一个公共的API啊
     [BurstCompile]
     public struct MousePickCollector : ICollector<RaycastHit>
     {
@@ -80,14 +83,17 @@ namespace Unity.Physics.MousePickBehaviour
     [UpdateInGroup(typeof(SimulationSystemGroup))]
     public class MousePickSystem : SystemBase
     {
+        //射线最远100米写死了
         const float k_MaxDistance = 100.0f;
 
+        //一个System里面就是得有一个EntityQuery来获取各个entities
         EntityQuery m_MouseGroup;
         BuildPhysicsWorld m_BuildPhysicsWorldSystem;
 
         public NativeArray<SpringData> SpringDatas;
         public JobHandle? PickJobHandle;
-
+        
+        //点击物体需要给物体添加一个Spring。所以弹簧都不配在physics里面写个标准的么。
         public struct SpringData
         {
             public Entity Entity;
@@ -96,10 +102,12 @@ namespace Unity.Physics.MousePickBehaviour
             public float MouseDepth;
         }
 
-
+        //按下鼠标左键时需要执行的Job。
+        //所以这个Job一定要写在这个System的OnUpdate的Input.GetMouseButtonDown里面。
         [BurstCompile]
         struct Pick : IJob
         {
+            //把需要用到的变量定义出来
             [ReadOnly] public CollisionWorld CollisionWorld;
             [ReadOnly] public int NumDynamicBodies;
             public NativeArray<SpringData> SpringData;
@@ -110,28 +118,46 @@ namespace Unity.Physics.MousePickBehaviour
 
             public void Execute()
             {
+                //MousePickCollector 这玩意儿就类似于 RaycastInfo。但是竟然要自己写么。都没现成的？？？？
+                //public bool CastRay<T>(RaycastInput input, ref T collector) where T : struct, ICollector<RaycastHit>
+                //限制了结构体和继承了ICollector<RaycastHit>就行。
                 var mousePickCollector = new MousePickCollector(1.0f, CollisionWorld.Bodies, NumDynamicBodies);
                 mousePickCollector.IgnoreTriggers = IgnoreTriggers;
 
                 CollisionWorld.CastRay(RayInput, ref mousePickCollector);
+                //加这个if干啥.里面不是有断言么
                 if (mousePickCollector.MaxFraction < 1.0f)
                 {
-                    float fraction = mousePickCollector.Hit.Fraction;
-                    RigidBody hitBody = CollisionWorld.Bodies[mousePickCollector.Hit.RigidBodyIndex];
 
+                    float fraction = mousePickCollector.Hit.Fraction;
+
+                    //Bodies是一个存储所有刚体的NativeArray。直接拿出来就行
+                    RigidBody hitBody = CollisionWorld.Bodies[mousePickCollector.Hit.RigidBodyIndex];
+                    //目的是获取到物体身上被射线点中的点。
+                    //hitBody.WorldFromBody是物体的世界坐标。现在需要一个把物体坐标转换为世界坐标的旋转矩阵。
+                    //物体世界坐标 = （0，0，0）左乘 世界的旋转矩阵 。
+                    //世界的旋转矩阵 = 物体的世界坐标的逆。又因为是旋转矩阵。所以逆就等于转置。所以旋转矩阵 = 当前物体的世界坐标的转置。
                     Math.MTransform bodyFromWorld = Inverse(new Math.MTransform(hitBody.WorldFromBody));
+                    //mousePickCollector.Hit.Position 这个是被射中的物体的坐标系。
+                    //左乘一个世界旋转矩阵就能得到世界坐标了。
                     float3 pointOnBody = Mul(bodyFromWorld, mousePickCollector.Hit.Position);
 
+                    //以上步骤不打个包写个API出来么？直接写在射线里pointOnBody = mousePickCollector.Hit.Position.ToWorld不好么，不过这个Position是个float3..确实不能在float3里加
+
+
+                    //建立一个弹簧。。本质上是像有限元的思路
                     SpringData[0] = new SpringData
                     {
                         Entity = hitBody.Entity,
                         Dragging = 1,
                         PointOnBody = pointOnBody,
+                        //其实就是被点击物体的z值。
                         MouseDepth = Near + math.dot(math.normalize(RayInput.End - RayInput.Start), Forward) * fraction * k_MaxDistance,
                     };
                 }
                 else
                 {
+
                     SpringData[0] = new SpringData
                     {
                         Dragging = 0
@@ -142,6 +168,7 @@ namespace Unity.Physics.MousePickBehaviour
 
         public MousePickSystem()
         {
+            //不销毁
             SpringDatas = new NativeArray<SpringData>(1, Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
             SpringDatas[0] = new SpringData();
         }
@@ -166,12 +193,13 @@ namespace Unity.Physics.MousePickBehaviour
             {
                 return;
             }
-
+            //Dependcy需要设置
             var handle = JobHandle.CombineDependencies(Dependency, m_BuildPhysicsWorldSystem.GetOutputDependency());
 
             if (Input.GetMouseButtonDown(0) && (Camera.main != null))
             {
                 Vector2 mousePosition = Input.mousePosition;
+                //获取鼠标射线的方式还没变
                 UnityEngine.Ray unityRay = Camera.main.ScreenPointToRay(mousePosition);
 
                 var mice = m_MouseGroup.ToComponentDataArray<MousePick>(Allocator.TempJob);
@@ -205,7 +233,7 @@ namespace Unity.Physics.MousePickBehaviour
             {
                 SpringDatas[0] = new SpringData();
             }
-
+            //Dependency补充一下
             Dependency = handle;
         }
     }
@@ -258,14 +286,17 @@ namespace Unity.Physics.MousePickBehaviour
                 PhysicsMass massComponent = Masses[entity];
                 PhysicsVelocity velocityComponent = Velocities[entity];
 
+                //相当于Mass等于无穷大时的直接退出,massComponent也是离谱为啥不存mass存个InverseMass
                 if (massComponent.InverseMass == 0)
                 {
                     return;
                 }
-
+                //这个只是获取每帧物体的旋转和位置
                 var worldFromBody = new MTransform(Rotations[entity].Value, Positions[entity].Value);
 
                 // Body to motion transform
+                // 获取质心坐标。InertiaOrientation不懂，是PhysicsMass里独有的一个四元数
+                // 大概的意思是从物体的世界坐标变成了运动的世界坐标
                 var bodyFromMotion = new MTransform(Masses[entity].InertiaOrientation, Masses[entity].CenterOfMass);
                 MTransform worldFromMotion = Mul(worldFromBody, bodyFromMotion);
 
@@ -274,25 +305,30 @@ namespace Unity.Physics.MousePickBehaviour
                 velocityComponent.Linear *= gain;
                 velocityComponent.Angular *= gain;
 
-                // Get the body and mouse points in world space
+                // Get the body and mouse points in world spaces
                 float3 pointBodyWs = Mul(worldFromBody, springData.PointOnBody);
+                // 弹簧的世界坐标
                 float3 pointSpringWs = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, springData.MouseDepth));
 
                 // Calculate the required change in velocity
+                // 从物体坐标系又变成了运动坐标系？
                 float3 pointBodyLs = Mul(Inverse(bodyFromMotion), springData.PointOnBody);
                 float3 deltaVelocity;
                 {
+                    //弹簧和作用点的距离
                     float3 pointDiff = pointBodyWs - pointSpringWs;
                     float3 relativeVelocityInWorld = velocityComponent.Linear + math.mul(worldFromMotion.Rotation, math.cross(velocityComponent.Angular, pointBodyLs));
 
+                    //劲度系数
                     const float elasticity = 0.1f;
+                    //怎么定义这么多阻力
                     const float damping = 0.5f;
                     deltaVelocity = -pointDiff * (elasticity / Time.DeltaTime) - damping * relativeVelocityInWorld;
                 }
 
                 // Build effective mass matrix in world space
-                // TODO how are bodies with inf inertia and finite mass represented
-                // TODO the aggressive damping is hiding something wrong in this code if dragging non-uniform shapes
+                // TODO how are bodies with inf inertia and finite mass represented 都无穷大的惯性和质量了，就不存在相对应的物理效果了啊
+                // TODO the aggressive damping is hiding something wrong in this code if dragging non-uniform shapes 阻力很大的时候，不均匀的形状会发生错误。。emm
                 float3x3 effectiveMassMatrix;
                 {
                     float3 arm = pointBodyWs - worldFromMotion.Translation;
@@ -323,6 +359,7 @@ namespace Unity.Physics.MousePickBehaviour
 
                 // Clip the impulse
                 const float maxAcceleration = 250.0f;
+
                 float maxImpulse = math.rcp(massComponent.InverseMass) * Time.DeltaTime * maxAcceleration;
                 impulse *= math.min(1.0f, math.sqrt((maxImpulse * maxImpulse) / math.lengthsq(impulse)));
 
