dfcef23d3ebcc4b2b0ef79daacc264fa21202d24
jayIndieGame <17635351002@163.com>
2021-08-13 16:43:38 +0800

    Change Something

--- Assets/01_TraditionalProjects/00_TraditionalBasicPerformanceTest/Scripts/Move.cs
+++ Assets/01_TraditionalProjects/00_TraditionalBasicPerformanceTest/Scripts/Move.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Move : MonoBehaviour
 {
     [SerializeField] public GameObject cube;
     public int CountToGen;
-    // Start is called before the first frame update
-    void Start()
+
+
+
+    public void ButtonClickToTest()
     {
+        //SceneManager.LoadScene("Lerp");
+
         for (int i = 0; i < CountToGen; i++)
         {
             GameObject obj = Instantiate(cube, new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10)),
